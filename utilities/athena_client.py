8d55d419d64350729e084166164656ae90dd3c09
Emanuele-Ubuntu <ubuntu@ip-172-31-45-239.eu-central-1.compute.internal>
2020-08-31 16:22:55 +0000

    added region name

--- utilities/athena_client.py
+++ utilities/athena_client.py
@@ -6,22 +6,22 @@ import multiprocessing as mp
 import hashlib
 
 class athena_API():
-    def _get_connection_keys(self):
+    def _get_connection_keys(self, region_name):
         config = ConfigParser()
         config.read(os.path.expanduser('~/.dpcfg.ini'))
         return dict(
                 aws_access_key_id = config.get('athena','AccessKeyId'),
                 aws_secret_access_key = config.get('athena','SecretAccessKey'),
-                s3_staging_dir = 's3://aws-athena-query-results-724810233589-us-east-1',
-                region_name = 'us-east-1'
+                s3_staging_dir = f's3://aws-athena-query-results-724810233589-{region_name}',
+                region_name = region_name
                 )
 
-    def _get_db_connection(self):
-        keys = self._get_connection_keys()
+    def _get_db_connection(self, region_name):
+        keys = self._get_connection_keys(region_name)
         return pyathena.connect(**keys)
 
-    def get_pandas_df(self, query):
-        connection = self._get_db_connection()   
+    def get_pandas_df(self, query, region_name='us-east-1'):
+        connection = self._get_db_connection(region_name)   
         return pd.read_sql(query, connection)
 
     def get_SQL_query(self):
